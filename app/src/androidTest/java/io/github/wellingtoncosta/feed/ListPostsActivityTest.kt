94356e024bb714382ab272fc9cf0371464cfe372
Wellington Costa Pereira <wellington.costa128@gmail.com>
2019-07-22 15:49:47 -0300

    Refactoring instrumented tests using Robot pattern.

--- app/src/androidTest/java/io/github/wellingtoncosta/feed/ListPostsActivityTest.kt
+++ app/src/androidTest/java/io/github/wellingtoncosta/feed/ListPostsActivityTest.kt
@@ -1,9 +1,5 @@
 package io.github.wellingtoncosta.feed
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
@@ -12,10 +8,12 @@ import io.github.wellingtoncosta.feed.app.ui.listposts.ListPostsActivity
 import io.github.wellingtoncosta.feed.extension.asJson
 import io.github.wellingtoncosta.feed.extensions.dispatches
 import io.github.wellingtoncosta.feed.extensions.responses
-import org.hamcrest.CoreMatchers.not
-import org.junit.*
+import io.github.wellingtoncosta.feed.robot.listPosts
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-import android.content.Intent
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -24,26 +22,39 @@ class ListPostsActivityTest {
     @Rule @JvmField val activityRule = ActivityTestRule(ListPostsActivity::class.java, true, true)
 
     @Test fun shouldListWithEmptyResponse() {
+        listPosts(activityRule) {
+
+            dispatchEmptyResponse()
+
+            launch()
+
+            assertNoPosts()
+
+        }
+    }
+
+    @Test fun shouldListWithNonEmptyResponse() {
+        listPosts(activityRule) {
+
+            dispatchFivePostsResponse()
+
+            launch()
+
+            assertFivePosts()
+
+        }
+    }
+
+    private fun dispatchEmptyResponse() {
         server.dispatcher = dispatches { path ->
             when (path) {
                 "/posts" -> 200 responses "payloads/empty-list-response.json".asJson()
                 else -> 404 responses null
             }
         }
-
-        activityRule.launchActivity(Intent())
-
-        onView(withId(R.id.list_posts_root))
-            .check(matches(isDisplayed()))
-
-        onView(withId(R.id.no_posts_text))
-            .check(matches(isDisplayed()))
-
-        onView(withId(R.id.recycler_view))
-            .check(matches(not(isDisplayed())))
     }
 
-    @Test fun shouldListWithNonEmptyResponse() {
+    private fun dispatchFivePostsResponse() {
         server.dispatcher = dispatches { path ->
             when (path) {
                 "/posts" -> 200 responses "payloads/five-posts-response.json".asJson()
@@ -51,17 +62,6 @@ class ListPostsActivityTest {
                 else -> 404 responses null
             }
         }
-
-        activityRule.launchActivity(Intent())
-
-        onView(withId(R.id.list_posts_root))
-            .check(matches(isDisplayed()))
-
-        onView(withId(R.id.recycler_view))
-            .check(matches(isDisplayed()))
-
-        onView(withId(R.id.no_posts_text))
-            .check(matches(not(isDisplayed())))
     }
 
     companion object {
---
+++ app/src/androidTest/java/io/github/wellingtoncosta/feed/robot/ListPostsRobot.kt
@@ -0,0 +1,55 @@
+package io.github.wellingtoncosta.feed.robot
+
+import android.content.Intent
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import io.github.wellingtoncosta.feed.R
+import io.github.wellingtoncosta.feed.app.ui.listposts.ListPostsActivity
+import io.github.wellingtoncosta.feed.assertion.RecyclerViewItemCountAssertion.Companion.hasItemCount
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.not
+
+class ListPostsRobot(
+    private val activityRule: ActivityTestRule<ListPostsActivity>
+) {
+
+    fun assertNoPosts() {
+        onView(withId(R.id.list_posts_root))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.no_posts_text))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.recycler_view))
+            .check(matches(not(isDisplayed())))
+    }
+
+    fun assertFivePosts() {
+        onView(withId(R.id.list_posts_root))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.recycler_view))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.no_posts_text))
+            .check(matches(not(isDisplayed())))
+
+        onView(
+            allOf<View>(
+                withId(R.id.recycler_view),
+                isDescendantOfA(withId(R.id.list_posts_root))
+            )
+        ).check(hasItemCount(5))
+    }
+
+    fun launch(): Unit = activityRule.launchActivity(Intent()).let { Unit }
+
+}
+
+fun listPosts(
+    activityRule: ActivityTestRule<ListPostsActivity>,
+    body: ListPostsRobot.() -> Unit
+) = ListPostsRobot(activityRule).apply(body)
